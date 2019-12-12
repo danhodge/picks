9230c8b35906881bfa77ce3d031fa4ad2fe326bc
Dan Hodge <danhodge@gmail.com>
2019-12-11 21:19:23 -0500

    add teams table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_021528) do
+ActiveRecord::Schema.define(version: 2019_12_12_021833) do
 
   create_table "seasons", force: :cascade do |t|
     t.integer "year", null: false
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2019_12_12_021528) do
     t.datetime "updated_at", null: false
     t.index ["token"], name: "index_sessions_on_token", unique: true
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_teams_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
