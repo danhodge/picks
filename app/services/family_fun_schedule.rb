5909f721bdf7409e19072967159ac38eeba9f378
Dan Hodge <danhodge@gmail.com>
2019-12-20 22:29:16 -0500

    scrape picks

--- app/services/family_fun_schedule.rb
+++ app/services/family_fun_schedule.rb
@@ -26,7 +26,8 @@ class FamilyFunSchedule
 
   def create(results)
     Bowl.transaction do
-      results.each do |result|
+      games = []
+      results[:schedule].each do |result|
         bowl = Bowl.where(name: Bowl.normalize_name(result[:game])).first_or_create! do |b|
           b.city, b.state = result[:location].split(", ")
         end
@@ -43,9 +44,23 @@ class FamilyFunSchedule
           v.losses = result[:home_losses]
         end
 
-        Game.where(bowl: bowl, season: season, visitor: visiting_team, home: home_team).first_or_create! do |g|
+        game = Game.where(bowl: bowl, season: season, visitor: visiting_team, home: home_team).first_or_create! do |g|
           g.game_time = result[:time]
           g.game_type = Game::GAME_TYPE_REGULAR
+        end
+        games << game
+      end
+
+      results[:participants].each do |participant|
+        pt = Participant.where(season: season, nickname: participant[:name]).first_or_create! do |p|
+          p.tiebreaker = participant[:tie_breaker]
+        end
+
+        participant[:picks].zip(games).each do |pick, game|
+          pk = Pick.where(season: season, participant: pt, game: game).first_or_initialize
+          pk.team = game.teams.find { |t| t.name == Team.normalize_name(pick[:team]) }
+          pk.points = pick[:points]
+          pk.save!
         end
       end
     end
@@ -54,36 +69,37 @@ class FamilyFunSchedule
   def extract_picks(page)
     table = page.search('//table').first
 
-    result = []
+    schedule = []
+    result = { schedule: schedule, participants: [] }
 
     table.search('//tr').each_with_index do |row, i|
       case i
       when 0
         # 0 = games
-        row.search('td').drop(1).each { |cell| result << { game: cell.text } }
+        row.search('td').drop(1).each { |cell| schedule << { game: cell.text } }
       when 1
         # 1 = locations
-        row.search('td').drop(1).each_with_index { |cell, j| result[j][:location] = cell.text }
+        row.search('td').drop(1).each_with_index { |cell, j| schedule[j][:location] = cell.text }
       when 2
         # 2 = date/time
         row.search('td').drop(1).each_with_index do |cell, j|
           date_time = cell.children[0].children.map(&:text).values_at(0, 2).join(' ')
           game_year = date_time.start_with?('Dec') ? season.year : season.year + 1
-          result[j][:time] = DateTime.strptime("#{game_year} #{date_time} -0500", '%Y %b. %d %I:%M %p %z').iso8601
+          schedule[j][:time] = DateTime.strptime("#{game_year} #{date_time} -0500", '%Y %b. %d %I:%M %p %z').iso8601
         end
       # 3 = TV
       # 4 = Result
       when 5
         # 5 = teams
         row.search('td').drop(1).each_slice(2).each_with_index do |(visitor, home), j|
-          result[j][:visitor] = visitor.text
-          result[j][:home] = home.text
+          schedule[j][:visitor] = visitor.text
+          schedule[j][:home] = home.text
         end
       when 6
         # 6 = records
-        row.search('td').drop(1).each_slice(2).take(result.size).each_with_index do |(visitor, home), j|
-          result[j][:visitor_wins], result[j][:visitor_losses] = visitor.text.gsub(/\(/, '').gsub(/\)/, '').split('-').map(&:to_i)
-          result[j][:home_wins], result[j][:home_losses] = home.text.gsub(/\(/, '').gsub(/\)/, '').split('-').map(&:to_i)
+        row.search('td').drop(1).each_slice(2).take(schedule.size).each_with_index do |(visitor, home), j|
+          schedule[j][:visitor_wins], schedule[j][:visitor_losses] = visitor.text.gsub(/\(/, '').gsub(/\)/, '').split('-').map(&:to_i)
+          schedule[j][:home_wins], schedule[j][:home_losses] = home.text.gsub(/\(/, '').gsub(/\)/, '').split('-').map(&:to_i)
         end
       when -> (n) { n >= 8 }
         # rows 8 - end are participants
@@ -100,21 +116,21 @@ class FamilyFunSchedule
   attr_reader :base_url, :year_indicator, :logger, :agent, :season
 
   def handle_participant(result, cells)
-    picks = cells.drop(1).take(result.games.count * 2).each_slice(2).each_with_index.map do |(visitor, home), i|
+    picks = cells.drop(1).take(result[:schedule].count * 2).each_slice(2).each_with_index.map do |(visitor, home), i|
       visitor_points = visitor.text
       home_points = home.text
 
       if visitor_points.length > 0
-        Pick.new(result.games[i], result.games[i].visitor, visitor_points.to_i)
+        { team: result[:schedule][i][:visitor], points: visitor_points.to_i }
       elsif home_points.length > 0
-        Pick.new(result.games[i], result.games[i].home, home_points.to_i)
+        { team: result[:schedule][i][:home], points: home_points.to_i }
       end
     end
 
     name = cells[0].text
-    if picks.length == result.games.length && picks.compact.length == picks.length
-      tie_breaker = cells.drop(3 + (result.games.count * 2)).first.text
-      result.add_participant(Participant.new(name, tie_breaker, picks))
+    if picks.length == result[:schedule].length && picks.compact.length == picks.length
+      tie_breaker = cells.drop(3 + (result[:schedule].count * 2)).first.text
+      result[:participants] << { name: name, tie_breaker: tie_breaker, picks: picks }
     else
       logger.error "Ignoring participant: #{name} due to missing picks"
     end
